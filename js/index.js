5bd7df0bf5e20a1661b4ec5b9fbd47e33ae0a784
amirali1390 <amiralimozafar5@gmail.com>
2023-02-08 21:17:00 +0330

    ZXZXZ

--- js/index.js
+++ js/index.js
@@ -8,34 +8,34 @@ const icon4 = document.querySelectorAll("i")
 const serch =document.querySelector(".serch")
 
 icon.addEventListener("click", function(){
-nav[5].classList.toggle("menoopen");
+nav[6].classList.toggle("menoopen");
 icon1[0].classList.toggle("bar1new");
 icon2[1].classList.toggle("bar2new");
 icon2[2].classList.toggle ("bar3new");
 });
-nav[9].addEventListener("click" ,function(){
+nav[10].addEventListener("click" ,function(){
     meno.classList.add("menoglobenew")
        
-        nav[9].style.display ="none";
-        nav[10].style.display ="block";
+        nav[10].style.display ="none";
+        nav[11].style.display ="block";
    
 
 }) ;
-nav[10].addEventListener("click" ,function(){
+nav[11].addEventListener("click" ,function(){
     meno.classList.remove("menoglobenew")
        
-    nav[10].style.display ="none";
-    nav[9].style.display ="block";
+    nav[11].style.display ="none";
+    nav[10].style.display ="block";
    
 
 }) ;
-nav[7].addEventListener("click" ,function(){
+nav[8].addEventListener("click" ,function(){
 serch.classList.add("newsearch")
-        nav[7].style.display ="none";
-        nav[15].style.display ="block";
+        nav[8].style.display ="none";
+        nav[16].style.display ="block";
 });
-nav[15].addEventListener("click" ,function(){
+nav[16].addEventListener("click" ,function(){
     serch.classList.remove("newsearch")
-            nav[15].style.display ="none";
-            nav[7].style.display ="block";
+            nav[16].style.display ="none";
+            nav[8].style.display ="block";
     }); 
