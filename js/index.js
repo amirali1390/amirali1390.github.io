51c8c1b5518b90cacb7a780b17a0bcc8599addb2
amirali1390 <amiralimozafar5@gmail.com>
2023-02-10 18:44:59 +0330

    gfgfdg

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const icon3 = document.querySelectorAll("span");
 const meno  = document.querySelector(".menoglobe")
 const icon4 = document.querySelectorAll("i")
 const serch =document.querySelector(".serch")
+const body =document.querySelector("body")
 
 icon.addEventListener("click", function(){
 nav[6].classList.toggle("menoopen");
@@ -13,29 +14,31 @@ icon1[0].classList.toggle("bar1new");
 icon2[1].classList.toggle("bar2new");
 icon2[2].classList.toggle ("bar3new");
 });
-nav[10].addEventListener("click" ,function(){
+nav[17].addEventListener("click" ,function(){
     meno.classList.add("menoglobenew")
        
-        nav[10].style.display ="none";
-        nav[11].style.display ="block";
+        nav[17].style.display ="none";
+        nav[18].style.display ="block";
    
 
 }) ;
-nav[11].addEventListener("click" ,function(){
+nav[18].addEventListener("click" ,function(){
     meno.classList.remove("menoglobenew")
        
-    nav[11].style.display ="none";
-    nav[10].style.display ="block";
+    nav[18].style.display ="none";
+    nav[17].style.display ="block";
    
 
 }) ;
-nav[8].addEventListener("click" ,function(){
+nav[15].addEventListener("click" ,function(){
 serch.classList.add("newsearch")
-        nav[8].style.display ="none";
-        nav[16].style.display ="block";
+        nav[15].style.display ="none";
+        nav[11].style.display ="block";
+      
 });
-nav[16].addEventListener("click" ,function(){
+nav[11].addEventListener("click" ,function(){
     serch.classList.remove("newsearch")
-            nav[16].style.display ="none";
-            nav[8].style.display ="block";
+            nav[11].style.display ="none";
+            nav[15].style.display ="block";
+   
     }); 
